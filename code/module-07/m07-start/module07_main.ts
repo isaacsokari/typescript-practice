19cdc7103f858c9e97d7623928c64e3b61a9c138
Isaac Sokari <isaacsokari@gmail.com>
2021-04-09 01:28:59 +0100

    modules and namespaces in typescript

--- code/module-07/m07-start/module07_main.ts
+++ code/module-07/m07-start/module07_main.ts
@@ -2,9 +2,17 @@
     Lab Start */
 
 /*  TODO Add the import statement. */
+import * as LoanPrograms from './module07_loan-programs';
 
 /*  TODO Update the function calls. */
-let interestOnlyPayment = calculateInterestOnlyLoanPayment({principle: 30000, interestRate: 5});
-let conventionalLoanPayment = calculateConventionalLoanPayment({principle: 30000, interestRate: 5, months: 180});
-console.log(interestOnlyPayment);         //* Returns "The interest only loan payment is 125.00" 
-console.log(conventionalLoanPayment);     //* Returns "The conventional loan payment is 237.24"
+let interestOnlyPayment = LoanPrograms.calculateInterestOnlyLoanPayment({
+  principle: 30000,
+  interestRate: 5,
+});
+let conventionalLoanPayment = LoanPrograms.calculateConventionalLoanPayment({
+  principle: 30000,
+  interestRate: 5,
+  months: 180,
+});
+console.log(interestOnlyPayment); //* Returns "The interest only loan payment is 125.00"
+console.log(conventionalLoanPayment); //* Returns "The conventional loan payment is 237.24"
